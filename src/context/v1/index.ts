0fbc8e8b620f4b8dec0530688e923cbd75cd8738
Andre Noberto <andre-noberto@hotmail.com>
2019-02-24 11:30:54 -0300

    Router handler for all subscriptions

--- src/context/v1/index.ts
+++ src/context/v1/index.ts
@@ -1,5 +1,9 @@
 import * as Express from 'express';
 
+import Subscriptions from '@api/v1/subscriptions';
+
 const router = Express.Router();
+
+router.use('/subscriptions', Subscriptions);
 
 export default router;
