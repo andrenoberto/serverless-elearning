e419fb29c992f41f5e7a7c3d3e18711b316b6adf
Andre Noberto <andre-noberto@hotmail.com>
2019-03-04 13:31:23 -0300

    Master key validation function moved to utils lib

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,5 +1,12 @@
+import {Config} from '@config/environment';
+
 export const toPascalCase = (str: string): string => {
   return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
     return letter.toUpperCase();
   }).replace(/\s+/g, '');
+};
+
+export const validateMasterKey = (req): boolean => {
+  const {master} = Config.factory();
+  return req.body.key === master.key;
 };
