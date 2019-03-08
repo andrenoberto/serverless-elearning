3e9f654d21018398e51b9fc85ee81fcdc0445ab6
Andre Noberto <andre-noberto@hotmail.com>
2019-03-07 21:23:18 -0300

    Safe navigating through responses

--- src/models/factories/subscription-factory.ts
+++ src/models/factories/subscription-factory.ts
@@ -1,52 +1,45 @@
 import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+import {oc} from 'ts-optchain';
 
 import {IDynamoSubscriptionItem} from '@db/interfaces';
 import {ISubscription, ISubscriptionScanResult} from '@models/interfaces/i-subscription';
 
 export class SubscriptionFactory {
   public static convertGetItemFromDynamoDB(getItemOutput: DynamoDB.Types.GetItemOutput): ISubscription {
-    let subscription: ISubscription = {};
-    if (getItemOutput && getItemOutput.Item) {
-      subscription = {
-        uuid: getItemOutput.Item.Uuid.S,
-        active: getItemOutput.Item.Active.BOOL,
-        name: getItemOutput.Item.Name.S,
-        description: getItemOutput.Item.Description.S,
-        days: parseInt(getItemOutput.Item.Days.N, 10),
-        price: parseFloat(getItemOutput.Item.Price.N),
-        userGroups: getItemOutput.Item.UserGroups.SS
-      };
-    }
-    return subscription;
+    return {
+      uuid: oc(getItemOutput).Item.Uuid.S(''),
+      active: oc(getItemOutput).Item.Active.BOOL() || false,
+      name: oc(getItemOutput).Item.Name.S(''),
+      description: oc(getItemOutput).Item.Description.S(''),
+      days: parseInt(oc(getItemOutput).Item.Days.N(), 10) || 0,
+      price: parseFloat(oc(getItemOutput).Item.Price.N()) || .0,
+      userGroups: oc(getItemOutput).Item.UserGroups.SS() || []
+    };
   }
 
   public static convertPutItemFromDynamoDB(putInput: DynamoDB.Types.PutItemInput): ISubscription {
-    let subscription: ISubscription = {};
-    if (putInput && putInput.Item) {
-      subscription = {
-        uuid: putInput.Item.Uuid.S,
-        active: putInput.Item.Active.BOOL,
-        name: putInput.Item.Name.S,
-        description: putInput.Item.Description.S,
-        days: parseInt(putInput.Item.Days.N, 10),
-        price: parseFloat(putInput.Item.Price.N),
-        userGroups: putInput.Item.UserGroups.SS
-      };
-    }
-    return subscription;
+    return {
+      uuid: oc(putInput).Item.Uuid.S(''),
+      active: oc(putInput).Item.Active.BOOL() || false,
+      name: oc(putInput).Item.Name.S(''),
+      description: oc(putInput).Item.Description.S(''),
+      days: parseInt(oc(putInput).Item.Days.N(''), 10) || 0,
+      price: parseFloat(oc(putInput).Item.Price.N('')) || .0,
+      userGroups: oc(putInput).Item.UserGroups.SS() || []
+    };
   }
 
   public static convertScanFromDynamoDB(scanOutput: DynamoDB.Types.ScanOutput): ISubscriptionScanResult {
     const subscriptions: Array<ISubscription> = [];
     scanOutput.Items.forEach((item: IDynamoSubscriptionItem) => {
       subscriptions.push({
-        uuid: item.Uuid.S,
-        active: item.Active.BOOL,
-        name: item.Name.S,
-        description: item.Description.S,
-        days: parseInt(item.Days.N, 10),
-        price: parseFloat(item.Price.N),
-        userGroups: item.UserGroups.SS
+        uuid: oc(item).Uuid.S(''),
+        active: oc(item).Active.BOOL() || false,
+        name: oc(item).Name.S(''),
+        description: oc(item).Description.S(''),
+        days: parseInt(oc(item).Days.N(''), 10) || 0,
+        price: parseFloat(oc(item).Price.N('')) || .0,
+        userGroups: oc(item).UserGroups.SS() || []
       });
     });
     return {
@@ -61,13 +54,13 @@ export class SubscriptionFactory {
     let subscription: ISubscription = {};
     if (updateItemOutput && updateItemOutput.Attributes) {
       subscription = {
-        uuid: updateItemOutput.Attributes.Uuid.S,
-        active: updateItemOutput.Attributes.Active.BOOL,
-        name: updateItemOutput.Attributes.Name.S,
-        description: updateItemOutput.Attributes.Description.S,
-        days: parseInt(updateItemOutput.Attributes.Days.N, 10),
-        price: parseFloat(updateItemOutput.Attributes.Price.N),
-        userGroups: updateItemOutput.Attributes.UserGroups.SS
+        uuid: oc(updateItemOutput).Attributes.Uuid.S(''),
+        active: oc(updateItemOutput).Attributes.Active.BOOL() || false,
+        name: oc(updateItemOutput).Attributes.Name.S(''),
+        description: oc(updateItemOutput).Attributes.Description.S(''),
+        days: parseInt(oc(updateItemOutput).Attributes.Days.N(), 10) || 0,
+        price: parseFloat(oc(updateItemOutput).Attributes.Price.N()) || .0,
+        userGroups: oc(updateItemOutput).Attributes.UserGroups.SS() || []
       };
     }
     return subscription;
